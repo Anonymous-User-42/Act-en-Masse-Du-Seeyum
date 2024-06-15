0e660c0c53a5b032d2faf471ffa5d1c18e3901b0
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2024-06-15 17:00:57 -0400

    Completed the Project

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// import assets from "./questions-answers";
-
 let assets = {
+  //	Asset JSON Object containing Questions, Answers && Options
   questions: [
     "What is the Capital of Australia ?",
     "How many Feet are there in 75 Yards ?",
@@ -39,15 +38,70 @@ let assets = {
   ],
 };
 
-let optionAlphbets = ["A", "B", "C"];
+game(assets);
 
-for (question in assets.questions) {
-  console.log(`${assets.questions[question]}\n`);
-  console.log(`Options\n`);
-  for (option in assets.options[question]) {
-    console.log(
-      `${optionAlphbets[option]} ${assets.options[question][option]}`
-    );
+function game(param) {
+  let sumScore = 0; //	Initial game score - is subject to change
+  let retakeChances = 1; //	Standard 1 retake chance per game
+  let gameCycle = 1; //	Validates if the game has ended or not and consequently proceeds with the program
+
+  //	Game Initial Boilerplate Logs
+
+  console.log(`Are You Smarter than a 5th Grader\n\n`);
+  console.log(
+    `John- I finally found the gold!I can finally be rich and take care of my family!\nVince the Genie- Wait a minute, you need to answer 10 questions to get the full amount\nJohn- What! I never heard of this???\nAHHAHAHAHHAHAHAH!!! These questions are called are you smarter than a 5th grader,every correct question adds a million on to the chest, you get one re-try on the whole quiz\nI will give you a friend to help you.\nHelp John win the prize to save himself from the streets of Toronto and so he can take care of his family`
+  );
+  console.log("\n\n");
+
+  //	Game User Interaction Interface
+
+  let optionAlphbets = ["A", "B", "C"];
+
+  for (question in param.questions) {
+    console.log(`${param.questions[question]}\n`);
+    console.log(`Options\n`);
+    for (option in param.options[question]) {
+      console.log(
+        `${optionAlphbets[option]} ${param.options[question][option]}`
+      );
+    }
+    console.log("\n");
+    if (gameCycle !== 0) {
+      //	Game Logic
+
+      const input = require(`prompt-sync`)();
+      const inputStr = input(
+        `Enter your Answer Selection (Please enter the option choices, ie. not the Alphabets): `
+      ); //	User answer input
+
+      let validation =
+        inputStr.toLowerCase() === param.answer[question].toLowerCase(); //	Validates if the given answer choice is indeed right
+
+      if (validation) {
+        sumScore++;
+      } else if (retakeChances > 0) {
+        let retakeOrNot = input(`Do you want to retake this question (Y/N)`);
+
+        if (retakeOrNot.toLowerCase() === String(`y`)) {
+          retakeChances--;
+        } else {
+          //   throw new Error(`You have successfully exited the Game`); //	end game}
+          continue;
+        }
+      } else {
+        gameCycle = 0;
+      }
+    } else {
+      continue;
+    }
   }
-  console.log("\n");
+
+  //	Game Score Board
+
+  console.log("\n\n");
+  if (sumScore > 0) {
+    console.log(`Your Final Score is: ${sumScore} Million Ignots`);
+  } else {
+    console.log(`Your Final Score is: ${sumScore} Ignots`);
+  }
 }
